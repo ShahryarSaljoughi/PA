8990aaaf3aafd54bc63842763f932e90be3e04d5
shahryar saljoughi <s.saljoughi@outlook.com>
2023-01-14 00:13:10 +0330

    Add project files.

---
+++ PAClient/IndexEntry.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PAClient
+{
+    /// <summary>
+    /// Interaction logic for IndexEntry.xaml
+    /// </summary>
+    public partial class IndexEntry : Page
+    {
+        public IndexEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
