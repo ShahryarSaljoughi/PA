a0dfc4a924cd2cdd7134e8383ec2a749354bfe6f
shahryar saljoughi <s.saljoughi@outlook.com>
2023-01-27 20:52:34 +0330

    added .net 6 app

---
+++ PaDesktop/Core/PaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaDesktop.Core
+{
+    class PaDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(
+                "Data Source=pa.db;");
+            optionsBuilder.UseLazyLoadingProxies(true);
+        }
+    }
+}
